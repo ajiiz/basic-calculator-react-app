735197447002ba1a2894fcf0074e147f20b1d053
ajiiz <opticpiter@gmail.com>
2019-08-30 12:31:54 +0200

    Updates

---
+++ precache-manifest.43340e7d130f4ae9c3f01457d1c23d70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b062f1bd4f6143e74ab0a990efcd9a2f",
+    "url": "/basic-calculator-react-app/index.html"
+  },
+  {
+    "revision": "117079df9785a1c3f849",
+    "url": "/basic-calculator-react-app/static/css/main.cbef89ba.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/basic-calculator-react-app/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "117079df9785a1c3f849",
+    "url": "/basic-calculator-react-app/static/js/main.a40dfcc0.chunk.js"
+  },
+  {
+    "revision": "3b1800c0a4db2af42479",
+    "url": "/basic-calculator-react-app/static/js/runtime~main.0c07dcc6.js"
+  }
+]);
